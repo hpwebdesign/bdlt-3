1b8341bc2b334c6cccb825d9ba97a933584ad950
Agung Rifqi Hidayat <rayqiri@gmail.com>
2025-11-21 10:50:11 +0700

    Fix: language

--- upload/admin/language/en-gb/extension/module/bdlt_setting.php
+++ upload/admin/language/en-gb/extension/module/bdlt_setting.php
@@ -1,6 +1,6 @@
 <?php
-$_['heading_title2'] = "BDeepl Translation";
-$_['heading_title']  = '<span style="border-radius:2px; background-color: #2460b8; color: #fff; padding:2px 5px;">B</span> Deepl Translation';
+$_['heading_title2'] = "BDeepL Translation";
+$_['heading_title']  = '<span style="border-radius:2px; background-color: #2460b8; color: #fff; padding:2px 5px;">B</span> DeepL Translation';
 
 // Tab
 $_['tab_translation'] = 'Translation';
@@ -13,7 +13,7 @@ $_['entry_status']    = 'Status';
 $_['entry_heading_text']           = 'Heading';
 $_['entry_api_key']           	   = 'Deepl API Key';
 $_['entry_api_type']           	   = 'API Type Plan';
-$_['help_api_key']           	   = 'Please create your DeepL API Key and ensure your DeepL API is active. You can generate your API key from the DeepL Developer Dashboard. <a href="https://www.deepl.com/pro-account" target="_blank">CLICK HERE</a>.';
+$_['help_api_key']           	   = 'Please create your DeepL API Key and ensure your DeepL API is active. You can generate your API key from the DeepL Developer Dashboard. <a href="https://www.deepl.com/en/your-account/keys" target="_blank">CLICK HERE</a>.';
 $_['entry_translate_language']     = 'Default Translate to';
 $_['entry_paste_status']     	   = 'Paste And Translate';
 $_['entry_language_source']        = 'Default Language Source';
@@ -24,10 +24,10 @@ $_['entry_prompt_enhancements']    = 'Enhancements';
 $_['entry_prompt_creation']    	   = 'Creation';
 $_['entry_token']           	   = 'Token';
 $_['entry_sub_heading_text']       = 'Sub Heading';
-$_['placeholder_heading_text']     = 'Deepl Translation';
+$_['placeholder_heading_text']     = 'BDeepL Translation';
 $_['placeholder_sub_heading_text'] = 'Optionally insert custom rug size';
 $_['text_color_placeholder']       = 'Enter desired color (optional)';
-$_['text_success']                 = 'Success: You have modified Deepl Translation Setting!';
+$_['text_success']                 = 'Success: You have modified BDeepL Translation Setting!';
 $_['text_warning']                 = 'Custom rug added to cart not successfully!';
 $_['button_upgrade_database']      = 'Upgrade Database';
 $_['entry_patch_upgrade']          = 'Patch for upgrade';
@@ -41,14 +41,14 @@ $_['text_add_cron']                = 'Add cron job to execute following command:
 $_['text_uninstall']               = 'Uninstall';
 $_['text_success_trigger']         = 'Success: You have triggered changes!';
 $_['text_success_status']          = 'Success: Status has been changed!';
-$_['text_success_installed']       = 'Success: Deepl Translation Extension related table successfully installed!';
-$_['text_success_uninstalled']     = 'Success: Deepl Translation Extension related table uninstalled successfully!';
-$_['text_install_message']         = 'Missing Table Tables for <b>Deepl Translation</b>, Please Click Here to install it Now!';
-$_['text_uninstall_message']       = 'You have installed <b>Deepl Translation</b>. Click Here to uninstall it Now';
+$_['text_success_installed']       = 'Success: BDeepL Translation Extension related table successfully installed!';
+$_['text_success_uninstalled']     = 'Success: BDeepL Translation Extension related table uninstalled successfully!';
+$_['text_install_message']         = 'Missing Table Tables for <b>BDeepL Translation</b>, Please Click Here to install it Now!';
+$_['text_uninstall_message']       = 'You have installed <b>BDeepL Translation</b>. Click Here to uninstall it Now';
 $_['text_upgrade']                 = 'Install Table';
 $_['text_extension']               = "Extension";
 $_['entry_uninstall_table']        = 'Uninstall Table';
-$_['help_uninstall_table']         = 'Uninstall all table and settings of HP Deepl Translation';
+$_['help_uninstall_table']         = 'Uninstall all table and settings of HP BDeepL Translation';
 $_['button_uninstall_table']       = 'Uninstall Table';
 $_['entry_option']                 = 'Select Product Option.';
 $_['help_option']                  = 'Select product option that will be used to store
@@ -70,7 +70,7 @@ $_['text_validate_store']      = 'In order to use this extension, please validat
 $_['text_information_provide'] = '<p>Please provides the following information:</p>';
 $_['text_upgrade_version']     = 'New version of this extension is available. Download it <strong><a href="https://my.hpwebdesign.io" target="_blank">HERE</a></strong>';
 
-$_['error_permission'] = 'Warning: You do not have permission to modify module Deepl Translation!';
+$_['error_permission'] = 'Warning: You do not have permission to modify module BDeepL Translation!';
 $_['error_database']   = 'Table Not Found!';
 $_['error_translate_language']   = 'Translate Language not Set';
 $_['error_enhancements']   = 'Enhancements Error';
